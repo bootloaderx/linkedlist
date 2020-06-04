0599e42951d372514c1c456cacb83c8d273f5b71
Karahan <50397005+bootloaderx@users.noreply.github.com>
2020-06-04 23:49:27 +0300

    Update LinkedListOrnek.java

--- src/main/java/LinkedListOrnek.java
+++ src/main/java/LinkedListOrnek.java
@@ -2,21 +2,24 @@
 import java.util.LinkedList;
 public class LinkedListOrnek {
 	public static void main(String[] args) {
-		LinkedList<String> bagliListem = new LinkedList<String>();
-		bagliListem.addFirst("A");
-		bagliListem.add("B");
-		bagliListem.add("C");
-		bagliListem.add("D");
-		bagliListem.add(2, "K");//2. indekse K harfini ekledim
-		bagliListem.addLast("Z");
-		System.out.println("Listedeki elemanları silmeden önceki hâli");
-		System.out.println(bagliListem);
-		bagliListem.remove();
-		bagliListem.removeLast();
-		bagliListem.remove("C");
-		System.out.println("Listedeki elemanları sildikten sonraki hâli");
-		System.out.println(bagliListem);
-		System.out.println("Bağlı listedeki ilk eleman: "+ bagliListem.getFirst());
-		System.out.println("Bağlı listedeki son eleman: "+ bagliListem.peekLast());
+	LinkedList<String> bagliListem = new LinkedList<String>();
+	bagliListem.addFirst("A");
+	bagliListem.add("B");
+	bagliListem.add("C");
+	bagliListem.add("D");
+	bagliListem.add(2, "K"); //2. indekse K harfini ekledim
+	bagliListem.addLast("Z");
+
+	System.out.println("Listedeki elemanları silmeden önceki hâli");
+	System.out.println(bagliListem);
+
+	bagliListem.remove();
+	bagliListem.removeLast();
+	bagliListem.remove("C");
+
+	System.out.println("Listedeki elemanları sildikten sonraki hâli");
+	System.out.println(bagliListem);
+	System.out.println("Bağlı listedeki ilk eleman: "+ bagliListem.getFirst());
+	System.out.println("Bağlı listedeki son eleman: "+ bagliListem.peekLast());
 	}
 }
